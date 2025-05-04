7c388b48107ef65a00f8e4ae1e0231bf2132b107
Raviprabhat527 <ravikumar312004@gmail.com>
2025-05-04 11:27:08 +0530

    Advance Pattern Question

---
+++ class_6/Pattern10.java
@@ -0,0 +1,43 @@
+package class_6;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n=4;
+        
+           //Upper Half
+        for(int i=1; i<=n; i++){
+            //1st part
+         for(int j=1; j<=i; j++){
+            System.out.print("*");
+         }  
+         //Spaces
+          int Spaces = 2*(n-i);
+          for(int j=1; j<=Spaces; j++){
+            System.out.println(" ");
+          }
+           //2nd part
+           for(int j=1; j<=i; j++){
+            System.out.println("*");
+           }
+           System.out.println();
+        }
+
+        // Lower Half
+        for(int i=n; i>=1; i--){
+            //1st part
+         for(int j=1; j<=i; j++){
+            System.out.print("*");
+         }  
+         //Spaces
+          int Spaces = 2*(n-i);
+          for(int j=1; j<=Spaces; j++){
+            System.out.println(" ");
+          }
+           //2nd part
+           for(int j=1; j<=i; j++){
+            System.out.println("*");
+           }
+           System.out.println();
+        }
+    }
+}
---
+++ class_6/pattern13.java
@@ -0,0 +1,22 @@
+package class_6;
+
+public class pattern13 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.println(" ");
+            }
+            //1st half numbers
+           for(int j = i; j>=1; j--) {
+            System.out.print(j);
+           }
+           //2nd half numbers
+           for(int j=2; j<=i; j++){
+            System.out.print(j);
+           }
+           System.out.println();
+        }
+    }
+}
